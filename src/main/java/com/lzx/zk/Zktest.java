1047fe54d0bcf4916b1bd0e748aff05ecf718f9a
liao202 <liao_zhenxing@foxmail.com>
2016-04-21 17:42:42 +0800

    zk zk

---
+++ src/main/java/com/lzx/zk/Zktest.java
@@ -0,0 +1,43 @@
+package com.lzx.zk;
+
+import java.io.IOException;
+
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.ZooDefs.Ids;
+
+public class Zktest {
+
+	public static void main(String[] args) {
+
+		try {
+			ZooKeeper zk = new ZooKeeper("localhost:2181", 3000, new Watcher() {
+
+				@Override
+				public void process(WatchedEvent event) {
+					System.out.println(event);
+
+				}
+
+			});
+			zk.create("/providers/s1", "hello".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+			Thread.sleep(10000);
+			zk.close();
+			System.out.println("over!");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (KeeperException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
